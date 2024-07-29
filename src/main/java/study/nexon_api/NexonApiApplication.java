19c6e679337ae28a74469cc05aa3c736221586b5
장동건 <wer4886@naver.com>
2024-07-29 20:14:37 +0900

    Initial commit

---
+++ src/main/java/study/nexon_api/NexonApiApplication.java
@@ -0,0 +1,13 @@
+package study.nexon_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexonApiApplication.class, args);
+	}
+
+}
